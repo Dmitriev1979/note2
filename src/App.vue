b92beebaf91b47476d1fbe36ad9e4260febcbf08
Dmitriev1979 <dmitriev_iliy@mail.ru>
2019-12-06 07:38:57 +0300

    coomit 1

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+   <h1>Записная книжка</h1>
+   <app-addtodo @add-todo="addTodo" ></app-addtodo>
+   <hr>
+   <app-todoList 
+   v-if="todos.length"
+   v-bind:todos="todos"
+   v-on:remove-todo="removeTodo"
+   ></app-todoList>
+   <h3 v-else>Записей нет</h3>
+  </div>
+</template>
+
+<script>
+import TodoList from './TodoList'
+import Addtodo from './Addtodo'
+export default {
+  name: 'app',
+  data () {
+    return {
+      todos:[
+        {id:1, title: 'Купить фисташки', complited: false},
+        {id:2, title: 'Купить лайм', complited: false},
+        {id:3, title: 'Купить виски', complited: false},
+      ]
+    }
+  },
+  components:{
+    'app-todoList': TodoList,
+    'app-addtodo': Addtodo
+  },
+  methods:{
+    removeTodo(id){
+      this.todos = this.todos.filter(t=>t.id !== id)
+    },
+    addTodo(todo){
+      this.todos.push(todo)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<ul>
+		
+		 <app-todoItem
+		 	v-for="(todo, i) in todos"
+		 	v-bind:todo="todo"
+		 	v-bind:index="i"
+		 	v-on:remove-todo="removeTodo"
+		 ></app-todoItem>
+		
+		</ul>
+	</div>
+</template>
+<script>
+import TodoItem from './Todoitem'
+	export default{
+		props:['todos'],
+		data(){
+			return{
+				
+			}
+		},
+		components:{
+			'app-todoItem': TodoItem
+		},
+		methods:{
+			removeTodo(id){
+				this.$emit('remove-todo', id)
+			}
+		}
+	}
+</script>
+<style scoped>
+	ul{
+		list-style:none;
+		margin:0;
+		padding:0;
+	}
+
+</style>
